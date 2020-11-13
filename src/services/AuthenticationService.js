5626828ba23313306135a621a705db26a10a69cb
lbaldy <>
2020-11-13 22:05:06 +0100

    integrate firebase, add real services code

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -1,5 +1,8 @@
+import auth from '@react-native-firebase/auth';
+
 export const loginWithEmailAndPassword = ({email, password}) => {
-  return new Promise((resolve, reject) => {
-    resolve({user: {uid: '1234'}});
-  });
+  // return new Promise((resolve, reject) => {
+  //   resolve({user: {uid: '1234'}});
+  // });
+  return auth().signInWithEmailAndPassword(email, password);
 };
--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -1,15 +1,36 @@
+import firestore from '@react-native-firebase/firestore';
+
 export const saveTodo = ({uid, body, id}) => {
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
+  // return new Promise((resolve, reject) => {
+  //   resolve();
+  // });
+  if (id) {
+    return firestore().collection('Todos').doc(id).update({body});
+  }
+
+  return firestore().collection('Todos').add({body, uid});
 };
 
 export const deleteTodo = ({id}) => {
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
+  // return new Promise((resolve, reject) => {
+  //   resolve();
+  // });
+  return firestore().collection('Todos').doc(id).delete();
 };
 
 export const getUserTodos = async ({uid}) => {
-  return [{body: 'Hello World!', uid: 1234}];
+  // return [{body: 'Hello World!', uid: 1234}];
+  const todosCollection = await firestore()
+    .collection('Todos')
+    .where('uid', '==', uid)
+    .get();
+  const todosArray = [];
+  todosCollection.forEach((todo) => {
+    todosArray.push({
+      ...todo.data(),
+      id: todo.id,
+    });
+  });
+
+  return todosArray;
 };
